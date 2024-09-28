dda5206092c995f43465c256baf72b97433c6158
Thoufeek23 <mali21@binghamton.edu>
2024-09-28 08:49:58 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var nb;
+var carb;
+var casb;
+var nuteb;
+var wcb;
+
+function Nutty_Brownie(){
+    nb+=1;
+    alert("Nutty Brownie added to cart!");
+}
+function Caramel_Brownie(){
+    carb+=1;
+    alert("Caramel Brownie added to cart!");
+}
+function Cashew_Brownie(){
+    casb+=1;
+    alert("Cashew Brownie added to cart!");
+}
+function Nutella_Brownie(){
+    nuteb+=1;
+    alert("Nutella Brownie added to cart!");
+}
+function White_Chocolate_Brownie(){
+    wcb+=1;
+    alert("White Cocolate Brownie added to cart!");
+}
